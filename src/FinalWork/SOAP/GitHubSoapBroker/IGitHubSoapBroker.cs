cf02f970b30df2a1ac4abbe54c6a483e5dd718de
Sandra Fernandes <sandrapatfer@gmail.com>
2012-01-18 01:22:54 +0000

    update

--- src/FinalWork/SOAP/GitHubSoapBroker/IGitHubSoapBroker.cs
+++ src/FinalWork/SOAP/GitHubSoapBroker/IGitHubSoapBroker.cs
@@ -11,30 +11,37 @@ namespace GitHubSoapBroker
     {
         [OperationContract]
         [FaultContract(typeof(ServiceUnavailableFault))]
+        [FaultContract(typeof(InternalServerFault))]
         IssuesCollectionResp GetAllIssues();
 
         [OperationContract]
         [FaultContract(typeof(ServiceUnavailableFault))]
+        [FaultContract(typeof(InternalServerFault))]
         IssuesCollectionResp GetRepositoryIssues(string repository);
 
         [OperationContract]
         [FaultContract(typeof(ServiceUnavailableFault))]
-        RepositoriesCollectionResp GetRepositories();
-
-        [OperationContract]
-        [FaultContract(typeof(ServiceUnavailableFault))]
+        [FaultContract(typeof(InternalServerFault))]
         CreationStatusResp CreateIssue(string repository, IssueData i);
 
         [OperationContract]
         [FaultContract(typeof(ServiceUnavailableFault))]
+        [FaultContract(typeof(InternalServerFault))]
         StatusResp EditIssue(string repository, string id, IssueData i);
 
         [OperationContract]
         [FaultContract(typeof(ServiceUnavailableFault))]
+        [FaultContract(typeof(InternalServerFault))]
+        RepositoriesCollectionResp GetRepositories();
+
+        [OperationContract]
+        [FaultContract(typeof(ServiceUnavailableFault))]
+        [FaultContract(typeof(InternalServerFault))]
         CreationStatusResp CreateRepository(RepositoryData r);
 
         [OperationContract]
         [FaultContract(typeof(ServiceUnavailableFault))]
+        [FaultContract(typeof(InternalServerFault))]
         StatusResp EditRepository(string id, RepositoryData r);
     }
 }
